d5f37940ad3ca7948ef586749b4bb330e5f4aace
Lindsey Ulmer <lulmer@uw.edu>
2020-12-09 11:30:38 -0800

    updated file paths to be run from the main directory

--- CovidVoting/test/test_add_data.py
+++ CovidVoting/test/test_add_data.py
@@ -4,8 +4,10 @@ test_oneshot(self)
 test_smoke(self)
 test_edge(self)
 """
-import unittest
-from CovidElection import add_data
+import unittest, sys, os
+sys.path.append("..")
+print(os.getcwd())
+from CovidVoting.add_data import (add_data)
 #Define all states
 allstates=["Maryland", "Iowa", "Delaware", "Ohio", "Pennsylvania", "Nebraska", "Washington",
         "Alabama", "Arkansas", "New Mexico", "Texas", "California", "Kentucky", "Georgia",
@@ -19,6 +21,9 @@ allstates=["Maryland", "Iowa", "Delaware", "Ohio", "Pennsylvania", "Nebraska", "
 key=["Arizona", "Florida", "Georgia", "Michigan", "Minnesota", "North Carolina", "Ohio",
          "Pennsylvania", "Texas", "Wisconsin"]
 class TestAddData(unittest.TestCase):
+    """
+    This class defines the tests for add_data.py.
+    """
     def test_smoke(self):
         """Smoke Tests
         Args:
@@ -27,17 +32,19 @@ class TestAddData(unittest.TestCase):
             True: Test passed
             False: Test failed
         """
-        add_data.add_data('data/basedata.csv','data/raw_3_2020election.csv',"NAME","States",
-        allstates, "data/coviddataand2020Election.csv")
+    # add_data.add_data('data/basedata.csv','data/raw_3_2020election.csv',"NAME","States",
+    # allstates, "coviddataand2020Election.csv")
+        add_data('data/coviddataand2020Election.csv', 'data/raw_7_keystates_covid_voting_issue_poll.csv',
+       'NAME','States', key, "keystates_covid_2020voting_poll.csv")
     def test_oneshot(self):
         """One shot tests
-    Args:
-        self
-    Returns:
-        True: Test passed
-        False: Test failed
-    """
-        self.assertEqual(add_data.add_data('data/coviddataand2020Election.csv',
+        Args:
+            self
+        Returns:
+            True: Test passed
+            False: Test failed
+        """
+        self.assertEqual(add_data('data/coviddataand2020Election.csv',
         'data/raw_7_keystates_covid_voting_issue_poll.csv',
         'NAME','States', key, "keystates_covid_2020voting_poll.csv"),"data/keystates_covid_2020voting_poll.csv" )
     def test_edge(self):
@@ -49,6 +56,10 @@ class TestAddData(unittest.TestCase):
             False: Test failed
         """
         with self.assertRaises(TypeError):
-            add_data.add_data('data/basedata.csv','data/raw_3_2020election.csv',"wrongname",
+            add_data('data/basedata.csv','data/raw_3_2020election.csv',"wrongname",
             "States", allstates, "testresults.csv")
-unittest.main()
+if __name__ == '__main__':
+
+
+    unittest.main()
+
