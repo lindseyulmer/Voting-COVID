28da91a9176a57e84fba7b4355bf6a2bc67c77dc
lindseyulmer <68305716+lindseyulmer@users.noreply.github.com>
2020-12-11 15:37:52 -0800

    Update make_plot_swingstate.py

--- CovidVoting/make_plot_swingstate.py
+++ CovidVoting/make_plot_swingstate.py
@@ -11,7 +11,6 @@ from bokeh.models import (ColorBar, ColumnDataSource,
 from bokeh.plotting import figure
 from bokeh.transform import dodge, factor_cmap
 from bokeh.plotting import figure
-from bokeh.models import Div, Paragraph, Row, Column
 from bokeh.util.browser import view
 from jinja2 import Template
 
@@ -30,52 +29,51 @@ def make_plot_map(source_df, shapefile, field, range_col, hover_list, title):
     field: (df's col -> int) the variable used to fill colors
     range_col: (df's col -> int) the variable used to map numbers in a range,
     into a sequence of colors (the condition of results)
-    hover_list: (list) 
+    hover_list: (list)
     title: (string)the title of the map
     '''
 
     # Merge shapefile with covid data
-    map_info = shapefile.merge(source_df, left_on = "NAME", right_on = "state")
+    map_info = shapefile.merge(source_df, left_on="NAME", right_on="state")
     # Drop Alaska and Hawaii
     map_info = map_info.loc[~map_info['NAME'].isin(['Alaska', 'Hawaii'])]
     # Input GeoJSON source that contains features for plotting
-    geosource = GeoJSONDataSource(geojson = map_info.to_json())
+    geosource = GeoJSONDataSource(geojson=map_info.to_json())
 
     base_colors = ["#cb181d", "#fb6a4a", "#fcae91", "#fee5d9",
                    "#eff3ff", "#bdd7e7", "#6baed6", "#2171b5"]
     # Instantiate LinearColorMapper that linearly maps numbers in a range, into a sequence of colors.
-    color_mapper = LinearColorMapper(palette = base_colors,
-                                     low = source_df[range_col].min(),
-                                     high = source_df[range_col].max())
+    color_mapper = LinearColorMapper(palette=base_colors,
+                                     low=source_df[range_col].min(),
+                                     high=source_df[range_col].max())
 
     # Define custom tick labels for color bar.
     tick_labels = {'-8': 'Trump wins',
-                   '-6':'',
-                   '-4':'',
-                   '-2':'', 
-                   '2':'',
-                   '4':'',
-                   '6':'',
+                   '-6': '',
+                   '-4': '',
+                   '-2': '',
+                   '2': '',
+                   '4': '',
+                   '6': '',
                    '8':'Biden wins'}
 
     # Create color bar.
-    color_bar = ColorBar(color_mapper=color_mapper, 
+    color_bar = ColorBar(color_mapper=color_mapper,
                          label_standoff=5,
                          width=200, height=10,
                          border_line_color=None,
-                         location=(0,0), 
+                         location=(0,0),
                          orientation='horizontal',
-                         major_label_overrides=tick_labels
-                        )
+                         major_label_overrides=tick_labels)
 
     # Create figure object
     p = figure(title=title, 
-                      plot_height=400,
-                      plot_width=600,
-                      toolbar_location=None
-                      #toolbar_location='below',
-                      #tools="pan, wheel_zoom, box_zoom, reset"
-                      )
+               plot_height=400,
+               plot_width=600,
+               toolbar_location=None
+               # toolbar_location='below',
+               # tools="pan, wheel_zoom, box_zoom, reset"
+              )
     p.xgrid.grid_line_color = None
     p.ygrid.grid_line_color = None
 
@@ -83,32 +81,33 @@ def make_plot_map(source_df, shapefile, field, range_col, hover_list, title):
     states = p.patches('xs','ys', source=geosource,
                                fill_color={'field' :field,
                                            'transform' : color_mapper},
-                               line_color="gray", 
-                               line_width=0.25, 
-                               fill_alpha=1)
+                       line_color="gray",
+                       line_width=0.25,
+                       fill_alpha=1)
     # Create hover tool
     p.add_tools(HoverTool(renderers=[states], tooltips=hover_list))
 
     # Specify layout
     p.add_layout(color_bar, 'below')
-    #p.title.text_color = "#7D3C98"
+    # p.title.text_color = "#7D3C98"
     p.title.text_font_size = "15px"
-    #p.border_fill_color = "whitesmoke"
+    # p.border_fill_color = "whitesmoke"
     p.background_fill_color = "beige"
     sub_text = Title(text="", align='left',
-                     text_font_size='12px', text_color = "#A6ACAF")
+                     text_font_size='12px', text_color="#A6ACAF")
     p.add_layout(sub_text, 'below')
-    
+
     return p
 
 
 def make_plot_scatter(source_df, category_list, color_col, color_palette,
-                      x_col, y_col, hover_list, x_label, y_label, title, subtitle):
+                      x_col, y_col, hover_list,
+                      x_label, y_label, title, subtitle):
     """
     function: make a scatter plot to show the relationship
     among COVID-19 positive cases,
     deaths and election results in the swing states
-              
+        
     source_df: (dataframe)
     category_list: (list)
     color_col, x_col, y_col:
@@ -118,7 +117,7 @@ def make_plot_scatter(source_df, category_list, color_col, color_palette,
     title
     subtitle
     """
-    p = figure(plot_height = 400, toolbar_location=None)
+    p = figure(plot_height=400, toolbar_location=None)
     p.scatter(x=x_col, y=y_col,
               source=source_df,
               color=factor_cmap(color_col,
@@ -132,12 +131,12 @@ def make_plot_scatter(source_df, category_list, color_col, color_palette,
     hover = HoverTool()
     hover.tooltips = hover_list
     p.background_fill_color = "beige"
-    
+
     p.add_tools(hover)
     p.legend.location = "top_left"
-    #p.border_fill_color = "whitesmoke"
+    # p.border_fill_color = "whitesmoke"
     p.title.text_font_size = "15px"
-    
+
     sub_text = Title(text=subtitle, align='left',
                      text_font_size='12px', text_color="#A6ACAF")
     p.add_layout(sub_text, 'above')
@@ -158,7 +157,7 @@ def make_plot_bar(source_df, x_axis_list, title, y1,
     x_axis_list: (list)
     y1, y2: (string)
     y1_label, y2_label: (string)
-    hover_list: (list) 
+    hover_list: (list)
     title: (string)the title of the bar chart
 
     '''
@@ -196,10 +195,11 @@ def make_plot_bar(source_df, x_axis_list, title, y1,
     return p
 
 
-def make_plot_time_series(source_df, group_col, use_col, y_label, title, hover_list):
+def make_plot_time_series(source_df, group_col, use_col,
+                          y_label, title, hover_list):
     '''
-    function: make a time series chart to show the total cumulative cases for states where
-              each party won in the election
+    function: make a time series chart to show the total cumulative
+              cases for states where each party won in the election
     source_df: (df)
     group_col: (string) group by date and what 
     use_col: (string) the variables to show in the plot
@@ -227,10 +227,10 @@ def make_plot_time_series(source_df, group_col, use_col, y_label, title, hover_l
 
     # Render the race as step lines
     p.line('date', use_col, line_width=3,
-           color="#5DADE2", legend_label='Democratic', 
+           color="#5DADE2", legend_label='Democratic',
            source=d_cds)
     p.line('date', use_col, line_width=3,
-           color="#EC7063", legend_label='Republican', 
+           color="#EC7063", legend_label='Republican',
            source=r_cds)
     hover = HoverTool(tooltips=hover_list,
                       formatters={'@date': 'datetime'})
