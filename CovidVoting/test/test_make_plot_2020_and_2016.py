ceb65229647aaf2135466293d6f243977a126a32
lindseyulmer <68305716+lindseyulmer@users.noreply.github.com>
2020-12-11 14:45:16 -0800

    Update test_make_plot_2020_and_2016.py

--- CovidVoting/test/test_make_plot_2020_and_2016.py
+++ CovidVoting/test/test_make_plot_2020_and_2016.py
@@ -3,16 +3,10 @@ This module contains a series of test cases to confirm the validity
 of the implementation of the make_plot_2020_and_2016.py module.
 Tests include smoke tests and edge tests.
 """
-import json
 import os
 import unittest
 import geopandas as gpd
 import pandas as pd
-from bokeh.io import output_notebook, show
-from bokeh.models import (ColorBar, GeoJSONDataSource, 
-                          HoverTool, LinearColorMapper)
-from bokeh.models.widgets import Panel, Tabs
-from bokeh.plotting import figure
 from CovidVoting.make_plot_2020_and_2016 import make_plot
 
 current_location = os.getcwd()
@@ -20,6 +14,7 @@ os.chdir(current_location)
 contiguous_usa = gpd.read_file("data/shapefiles/cb_2018_us_state_20m.shp")
 covid = pd.read_csv("data/raw_2_covid_latest.csv")
 df_election = pd.read_csv("data/use_election.csv")
+
 
 class UnitTests(unittest.TestCase):
     """
@@ -33,14 +28,14 @@ class UnitTests(unittest.TestCase):
         """
         p = make_plot(covid, contiguous_usa, df_election)
         self.assertEqual(str(type(p)), "<class 'bokeh.models.layouts.Tabs'>")
-    
+
     def test_edge(self):
         '''
         This is an edge test that proves our code catches a type error when
-        the take in parameter is invalid. 
+        the take in parameter is invalid.
         '''
         with self.assertRaises(TypeError):
             make_plot(covid, contiguous_usa, 1)
-    
+
     if __name__ == '__main__':
         unittest.main()
