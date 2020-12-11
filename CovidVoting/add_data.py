e7d241c226a95c8c91ca72a773ecb1a4a1b68cf7
lindseyulmer <68305716+lindseyulmer@users.noreply.github.com>
2020-12-11 13:35:01 -0800

    Update add_data.py

--- CovidVoting/add_data.py
+++ CovidVoting/add_data.py
@@ -1,31 +1,33 @@
 """This module creates a base data set that add_data() build on.
 add_data(startdata,newdatafile,startdatastate, newdatastate, states,
-	newfilename)
+        newfilename)
 The purpose of this module is to prepare data for visualization
 """
-#Import Packages
+# Import Packages
 import pandas as pd
 import geopandas as gpd
 # Read the coviddata from a csv into a dataframe
-coviddata=pd.read_csv('data/raw_2_covid_latest.csv', index_col=0)
+coviddata = pd.read_csv('data/raw_2_covid_latest.csv',
+                         index_col=0)
 # Read in shapefile and examine data
-contiguous_usa=gpd.read_file('data/shapefiles/cb_2018_us_state_20m.shp')
+contiguous_usa = gpd.read_file('data/shapefiles/cb_2018_us_state_20m.shp')
 # Merge shapefile with population data
 case_states=contiguous_usa.merge(coviddata, left_on = 'NAME',
-				 right_on='State/Territory')
+				 right_on = 'State/Territory')
 # Drop Alaska and Hawaii
-case_states=case_states.loc[~case_states['NAME'].isin(['Alaska', 'Hawaii',
-						       "Puerto Rico"])]
+case_states=case_states.loc[~case_states['NAME'].isin(['Alaska', 'Hawaii",
+                                  "Puerto Rico"])]
 case_states.to_csv('basedata.csv')
 case_states.head()
 #Define all states
 allstates=["Maryland", "Iowa", "Delaware", "Ohio", "Pennsylvania", "Nebraska", "Washington",
-	   "Alabama", "Arkansas", "New Mexico", "Texas", "California", "Kentucky", "Georgia", "Wisconsin",
-	   "Oregon", "Missouri", "Virginia", "Tennessee", "Louisiana", "New York", "Michigan", "Idaho",
-	   "Florida","Illinois", "Montana", "Minnesota", "Indiana","Massachusetts","Kansas","Nevada",
-	   "Vermont", "Connecticut", "New Jersey", "District of Columbia", "North Carolina", 
-	   "Utah", "North Dakota", "South Carolina", "Mississippi", "Colorado", "South Dakota", 
-	   "Oklahoma", "Wyoming", "West Virginia", "Maine", "New Hampshire", "Arizona", "Rhode Island"]
+           "Alabama", "Arkansas", "New Mexico", "Texas", "California", "Kentucky", "Georgia", "Wisconsin",
+           "Oregon", "Missouri", "Virginia", "Tennessee", "Louisiana", "New York", 
+           "Michigan", "Idaho", "Florida", "Illinois", "Montana", "Minnesota",
+           "Indiana", "Massachusetts", "Kansas", "Nevada", "Vermont", "Connecticut",
+           "New Jersey", "District of Columbia", "North Carolina", "Utah", "North Dakota",
+           "South Carolina", "Mississippi", "Colorado", "South Dakota", "Oklahoma", "Wyoming", 
+           "West Virginia", "Maine", "New Hampshire", "Arizona", "Rhode Island"]
 #Define key states
 key=["Arizona", "Florida", "Georgia", "Michigan", "Minnesota", "North Carolina", "Ohio",
          "Pennsylvania", "Texas", "Wisconsin"]
@@ -46,9 +48,9 @@ def add_data(startdata,newdatafile,startdatastate, newdatastate, states, newfile
     # Read the dataframe to add too
     basedata = pd.read_csv(startdata, index_col=0)
     #Read data being added as a dataframe
-    newdf=pd.read_csv(newdatafile)
+    newdf = pd.read_csv(newdatafile)
     #Find all non-desired states
-    dropstates=[]
+    dropstates = []
     for eachstate in basedata[startdatastate]:
         if eachstate not in states:
             dropstates.append(eachstate)
@@ -59,6 +61,6 @@ def add_data(startdata,newdatafile,startdatastate, newdatastate, states, newfile
     basedata = basedata.loc[~basedata[startdatastate].isin(dropstates)]
     newdf = newdf.loc[~newdf[newdatastate].isin(dropstates)]
     #Merge datasets
-    mergeddata=basedata.merge(newdf, left_on=startdatastate, right_on=newdatastate)
+    mergeddata = basedata.merge(newdf, left_on=startdatastate, right_on=newdatastate)
     #write out file
     mergeddata.to_csv(newfilename)
