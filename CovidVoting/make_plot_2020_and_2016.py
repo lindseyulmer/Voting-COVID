09f061014870d9c6be4e59fca775d7863f73c177
Yanyan Guan <guany4@uw.edu>
2020-12-14 16:55:50 -0800

    Make it compatible with add_data

--- CovidVoting/make_plot_2020_and_2016.py
+++ CovidVoting/make_plot_2020_and_2016.py
@@ -1,37 +1,28 @@
 """
 In this module, the function make_plot uses the package Bokeh to help creating
-interactive plots to show voting and Covid data. 
+interactive plots to show voting and Covid data.
 """
-import pandas as pd
 from bokeh.plotting import figure
 from bokeh.models.widgets import Panel, Tabs
 from bokeh.models import (ColorBar, GeoJSONDataSource,
                           HoverTool, LinearColorMapper)
 
-
-def make_plot(covid, contiguous_usa, df_election):
+def make_plot(df_covid_election, contiguous_usa):
     '''
     This function makes maps to show 2020 or 2016 election results
     and Covid data together. Users can choose to see which year's election
     result they want to see by clicking on the tabs provided.
 
     Args:
-    - covid: (csv) Covid data in a csv file
-    - contiguous_usa: (shp) to make a map
-    - df_election: (csv) election results data of 2020 and 2016 in a csv file
+    - df_covid_election: (csv) Covid and election data in a csv file
+    - contiguous_usa: (shp) shape data of the U.S. to make a map
 
     Returns:
     - plots: contains two plots and tabs on top
     '''
-    # Keep states which are in the shapefile contiguous_usa
-    df_covid = covid.loc[covid["State/Territory"].isin(contiguous_usa["NAME"])]
-    df_merge = pd.merge(left=df_covid, right=df_election,
-                        left_on='State/Territory', right_on='state')
     # Merge shapefile with covid data
-    map_info = contiguous_usa.merge(df_merge,
-                                    left_on="NAME", right_on="State/Territory")
-    # Drop Alaska and Hawaii
-    map_info = map_info.loc[~map_info['NAME'].isin(['Alaska', 'Hawaii'])]
+    map_info = contiguous_usa.merge(df_covid_election,
+                                    left_on="NAME", right_on="state")
     # Input GeoJSON source that contains features for plotting
     geosource = GeoJSONDataSource(geojson=map_info.to_json())
     base_colors = ["#cb181d", "#fb6a4a", "#fcae91",
@@ -39,11 +30,11 @@ def make_plot(covid, contiguous_usa, df_election):
     # Instantiate LinearColorMapper that linearly maps numbers in a range,
     # into a sequence of colors.
     color_mapper_2020 = LinearColorMapper(palette=base_colors,
-                                          low=df_election["color_2020"].min(),
-                                          high=df_election["color_2020"].max())
+                                          low=df_covid_election["color_2020"].min(),
+                                          high=df_covid_election["color_2020"].max())
     color_mapper_2016 = LinearColorMapper(palette=base_colors,
-                                          low=df_election["color_2016"].min(),
-                                          high=df_election["color_2016"].max())
+                                          low=df_covid_election["color_2016"].min(),
+                                          high=df_covid_election["color_2016"].max())
     tick_labels_2020 = {'-8': 'Trump wins', '-6': '', '-4': '', '-2': '',
                         '2': '', '4': '', '6': '', '8': 'Biden wins'}
     tick_labels_2016 = {'-8': 'Trump wins', '-6': '', '-4': '',
@@ -105,11 +96,13 @@ def make_plot(covid, contiguous_usa, df_election):
                                         ('Total deaths',
                                          '@{Total Deaths}')]))
     p2016.add_tools(HoverTool(renderers=[states_2016],
-                    tooltips=[('State', '@NAME'),
-                              ('Case Rate per 100000',
-                               '@{Case Rate per 100000}'),
-                              ('Confirmed cases', '@{Total Cases}'),
-                              ('Total deaths', '@{Total Deaths}')]))
+                              tooltips=[('State', '@NAME'),
+                                        ('Case Rate per 100000',
+                                         '@{Case Rate per 100000}'),
+                                        ('Confirmed cases',
+                                         '@{Total Cases}'),
+                                        ('Total deaths',
+                                         '@{Total Deaths}')]))
     # Specify layout
     p2020.add_layout(color_bar_2020, 'below')
     p2016.add_layout(color_bar_2016, 'below')
